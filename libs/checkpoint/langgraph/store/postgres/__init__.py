dc1e4d33424988fb8ca28a74d88b723dcd1ab4fd
fengbincao <1050430934@qq.com>
2026-06-07 22:00:13 +0800

    feat(langgraph): support postgres

---
+++ libs/checkpoint/langgraph/store/postgres/__init__.py
@@ -0,0 +1,6 @@
+from langgraph.store.postgres.aio import AsyncPostgresStore
+from langgraph.store.postgres.base import PoolConfig, PostgresStore
+# type: ignore  MC8yOmFIVnBZMlhtbTdua3VMRG1sb3c2TXpKUlNBPT06ZjNiNmQzMGU=
+
+__all__ = ["AsyncPostgresStore", "PoolConfig", "PostgresStore"]
+# noqa  MS8yOmFIVnBZMlhtbTdua3VMRG1sb3c2TXpKUlNBPT06ZjNiNmQzMGU=
